dba11d9823b1531c8f0b4310b4a68882c46e82d5
Koketso Modise <koketsom-external@rain.co.za>
2022-06-18 04:29:52 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./src/utils/geocode')
+const forecast = require('./src/utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Koektso Modise'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me!',
+        name: 'Koketso'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        HelpText: 'Help Text',
+        title: 'Help',
+        name: 'Koketso'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+    
+    res.send({
+        forecast: 'It is raining currently',
+        location: 'Johannesburg',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*' , (req, res) => {
+    res.render('404', {
+        title: '404 help',
+        name: 'Koketso Modise',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Koketso Modise',
+        errorMessage: 'Page Not Found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
