f29d1f3b51d86b254fa1b3e0d15d04d2d7b3f556
Koketso Modise <koketsom-external@rain.co.za>
2022-06-21 19:45:06 +0200

    Making changes to the out put on the weather app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' , But it feels like ' + body.current.feelslike)
+            callback(undefined,'The weather is Currently ' + body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' degrees out at the moment. But it feels like ' + body.current.feelslike + ' degrees out! The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
